6945c76058b6d93fc0f8ff4f5695b8e1b66d2de3
awarnes <alex.warnes@moovel.com>
2023-01-21 16:01:01 -0800

    Set menu to bottom of screen.

--- src/routes/UserRoute.tsx
+++ src/routes/UserRoute.tsx
@@ -3,10 +3,15 @@ import { Link, Navigate, Outlet } from 'react-router-dom';
 import {Menu, Segment} from 'semantic-ui-react';
 import { useAuth } from '../hooks/useAuth';
 import { AuthContextType } from '../types/@types.auth';
+import './UserRoute.css';
+
+const MENU_ITEM_SHOPPING = 'shopping';
+const MENU_ITEM_PRODUCTS = 'products';
+const MENU_ITEM_SETTINGS = 'settings';
 
 export default function UserRoute(props: any) {
   const {user, signOut} = useAuth() as AuthContextType;
-  const [activeItem, setActiveItem] = useState('shopping');
+  const [activeItem, setActiveItem] = useState(MENU_ITEM_SHOPPING);
 
   if (!user) {
     return <Navigate to='/' />;
@@ -17,27 +22,27 @@ export default function UserRoute(props: any) {
       <Segment attached='top'>
         <Outlet />
       </Segment>
-      <Menu>
+      <Menu widths={3} className='mobile-footer'>
         <Menu.Item
-          name='shopping'
-          active={activeItem === 'shopping'}
-          onClick={() => setActiveItem('shopping')}
+          name={MENU_ITEM_SHOPPING}
+          active={activeItem === MENU_ITEM_SHOPPING}
+          onClick={() => setActiveItem(MENU_ITEM_SHOPPING)}
           >
             <Link to={`/user/${user.id}/shopping`}>Shopping</Link>
         </Menu.Item>
 
         <Menu.Item
-          name='products'
-          active={activeItem === 'products'}
-          onClick={() => setActiveItem('products')}
+          name={MENU_ITEM_PRODUCTS}
+          active={activeItem === MENU_ITEM_PRODUCTS}
+          onClick={() => setActiveItem(MENU_ITEM_PRODUCTS)}
           >
             <Link to={`/user/${user.id}/products`}>Products</Link>
         </Menu.Item>
 
         <Menu.Item
-          name='settings'
-          active={activeItem === 'settings'}
-          onClick={() => setActiveItem('settings')}
+          name={MENU_ITEM_SETTINGS}
+          active={activeItem === MENU_ITEM_SETTINGS}
+          onClick={() => setActiveItem(MENU_ITEM_SETTINGS)}
           >
             <Link to={`/user/${user.id}/settings`}>Settings</Link>
         </Menu.Item>
